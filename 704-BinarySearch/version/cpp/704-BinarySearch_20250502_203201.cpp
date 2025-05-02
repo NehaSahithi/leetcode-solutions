8a0f2fe5d52e928e30923bd941a994bff8cda20d
Kesarapu Neha Sahithi <136419617+NehaSahithi@users.noreply.github.com>
2025-05-02 20:32:04 +0530

    Create file

---
+++ 704-BinarySearch/version/cpp/704-BinarySearch_20250502_203201.cpp
@@ -0,0 +1,25 @@
+// Last updated: 5/2/2025, 8:32:01 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high=n-1;
+        
+        
+        
+        while (low<=high){
+            int mid =low+((high-low)/2);
+            if (nums[mid]== target){
+                return mid;
+            }
+            else if (nums[mid]< target){
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+
+        }
+        return -1;
+    }
+};
