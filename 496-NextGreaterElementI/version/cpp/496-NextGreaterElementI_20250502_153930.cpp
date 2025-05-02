1a3bccccd96e49380e34c0117bc4f6c8e899b0d6
Kesarapu Neha Sahithi <136419617+NehaSahithi@users.noreply.github.com>
2025-05-02 15:39:32 +0530

    Create file

---
+++ 496-NextGreaterElementI/version/cpp/496-NextGreaterElementI_20250502_153930.cpp
@@ -0,0 +1,26 @@
+// Last updated: 5/2/2025, 3:39:30 PM
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) { 
+    stack <int> s;
+    unordered_map <int,int> hmap;
+    for (int i=0;i<nums2.size();i++){
+        while (!s.empty() && s.top()<nums2[i]){
+            hmap[s.top()] =nums2[i];
+            s.pop();
+        }
+        s.push(nums2[i]);
+    }
+    while (!s.empty()){
+        hmap[s.top()]=-1;
+        s.pop();
+    }
+    vector <int>ans;
+    for (auto i:nums1){
+        ans.push_back(hmap[i]);
+    }
+
+    return ans;
+
+    }
+};
