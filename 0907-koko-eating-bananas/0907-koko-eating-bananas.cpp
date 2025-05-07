f8e111dfd75ad0c5166fabae99970f35b2484612
Kesarapu Neha Sahithi <136419617+NehaSahithi@users.noreply.github.com>
2025-05-07 10:52:30 +0530

    Time: 13 ms (30.55%), Space: 23 MB (42.41%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+long long divd(vector<int> &nums,int x){
+  long long sum=0;
+    if (x>0){
+    for(int i=0;i<nums.size();i++){
+        long long div=((nums[i]+x-1)/x);
+        sum+=div;
+    }
+    }
+    return sum;
+}
+    int minEatingSpeed(vector<int>& a, int H) {
+    sort(a.begin(),a.end());
+    int s=1;
+    long long e=*max_element(a.begin(),a.end());
+    long long res=e;
+    while(s<=e){
+        int m=(s+e)/2;
+        long long z =divd(a,m);
+        if(z<=H){
+            res=m;
+            e=m-1;
+        }
+        else{
+            s=m+1;
+        }
+    }
+    
+    return res;
+}
+};
