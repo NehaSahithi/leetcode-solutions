15a3701e3bfa9fe281423d12dbdcbb0358fd953a
Kesarapu Neha Sahithi <136419617+NehaSahithi@users.noreply.github.com>
2025-05-07 11:52:08 +0530

    Create file

---
+++ 1328-BreakaPalindrome/version/cpp/1328-BreakaPalindrome_20250507_115206.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/7/2025, 11:52:06 AM
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+       int n=palindrome.size();
+	if (n <= 1) {
+            return "";
+        }
+
+        for (int i = 0; i < n / 2; ++i) {
+            if (palindrome[i] != 'a') {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+	palindrome[n - 1] = 'b';
+        return palindrome;
+    }
+};
