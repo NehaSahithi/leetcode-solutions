713dece09d3d1f14128c597977a61b781684bb3c
Kesarapu Neha Sahithi <136419617+NehaSahithi@users.noreply.github.com>
2025-05-07 11:56:58 +0530

    Create file

---
+++ 179-LargestNumber/version/cpp/179-LargestNumber_20250507_115656.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/7/2025, 11:56:56 AM
+bool well(string a, string b){
+	return a+b>b+a;
+}
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+       vector <string> he;
+       for(int nu:nums){
+		he.push_back(to_string(nu));
+	}
+	sort(he.begin(), he.end(), well);
+	string result = "";
+    for (const string& str : he) {
+        result += str;
+    }
+    if (result[0] == '0' && all_of(result.begin(), result.end(), [](char c){ return c == '0'; })) {
+    return "0";
+}
+	return result; 
+    }
+};
