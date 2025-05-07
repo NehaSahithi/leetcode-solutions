f368a42334a0b84a78da098a3f7d838c8455d0ae
Kesarapu Neha Sahithi <136419617+NehaSahithi@users.noreply.github.com>
2025-05-07 11:33:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (79.03%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for (int i=0;i<stones.size();i++){
+		for (int k=0;k<jewels.size();k++){
+			if(stones[i]==jewels[k]){
+				count++;
+            }}}
+return count;
+   }
+};
