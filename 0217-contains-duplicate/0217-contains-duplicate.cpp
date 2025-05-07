0ae8545c5a417d6e3d1171a0541093691fd38a04
Kesarapu Neha Sahithi <136419617+NehaSahithi@users.noreply.github.com>
2025-05-07 12:08:48 +0530

    Time: 29 ms (71.1%), Space: 73.6 MB (58.3%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+#pragma GCC optimize ("O2") // Optimization pragma - OUTSIDE the class
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+        return false;
+    }
+};
